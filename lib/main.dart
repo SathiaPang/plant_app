5918a4d5de9926a29b7f190f8f6399392955abd5
Sathia Pang <pangvichetsatya1@gmail.com>
2023-12-05 18:27:04 +0700

    Second Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:plant_app/constants.dart';
+import 'package:plant_app/ui/onboard.dart';
 
 void main() {
   runApp(const Myapp());
@@ -12,24 +13,26 @@ class Myapp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       // Must use always, and then start to write like that
-      home: HomeScreen(),
+      debugShowCheckedModeBanner: false,
+      title: 'OnBoarding Screen ',
+      home: OnBoardinScreen(),
     );
   }
 }
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+// class HomeScreen extends StatelessWidget {
+//   const HomeScreen({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Hello there",
-          style: TextStyle(color: Colors.white),
-        ),
-        backgroundColor: Constants.primaryColor,
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(
+//           "Hello there",
+//           style: TextStyle(color: Colors.white),
+//         ),
+//         backgroundColor: Constants.primaryColor,
+//       ),
+//     );
+//   }
+// }
