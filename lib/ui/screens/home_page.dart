16aba824ae14c33ede3e019bc331ed77e6fa73a3
Sathia Pang <pangvichetsatya1@gmail.com>
2023-12-14 16:29:31 +0700

    Updated the new plant

--- lib/ui/screens/home_page.dart
+++ lib/ui/screens/home_page.dart
@@ -228,7 +228,59 @@ class _HomePageState extends State<HomePage> {
                     margin: EdgeInsets.only(bottom: 10, top: 10),
                     width: size.width,
                     child: Row(
-                      
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Stack(
+                          clipBehavior: Clip.none,
+                          children: [
+                            Container(
+                              width: 60.0,
+                              height: 60.0,
+                              decoration: BoxDecoration(
+                                color: Constants.primaryColor.withOpacity(.8),
+                                shape: BoxShape.circle,
+                              ),
+                            ),
+                            Positioned(
+                              bottom: 5,
+                              left: 0,
+                              right: 0,
+                              child: SizedBox(
+                                height: 80.0,
+                                child: Image.asset(_plantList[index].imageURL),
+                              ),
+                            ),
+                            Positioned(
+                              bottom: 5,
+                              left: 80,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(_plantList[index].category),
+                                  Text(
+                                    _plantList[index].plantName,
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 18,
+                                        color: Constants.blackColor),
+                                  ),
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                        Container(
+                          padding: EdgeInsets.only(right: 10),
+                          child: Text(
+                            r'$' + _plantList[index].price.toString(),
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18,
+                                color: Constants.primaryColor),
+                          ),
+                        )
+                      ],
                     ),
                   );
                 },
